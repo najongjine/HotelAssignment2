f40b81ad8365e20573279d1138572f6e4b0c758b
najongjine <najongjin3@hotmail.com>
2021-03-27 15:37:28 +0900

    .

---
+++ HotelAssignment2/Model/BlazorAmenitie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelAssignment2.Model
+{
+  public class BlazorAmenitie
+  {
+    public int Id { get; set; }
+    public string name{get;set;}
+    public string description { get; set; }
+  }
+}
