2852e8a41ec6aed8de3d5279c8c2ba9ca31c0bf3
najongjine <najongjin3@hotmail.com>
2021-04-09 17:51:05 +0900

    .

---
+++ Models/HotelRoomDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+  public class HotelRoomDTO
+  {
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public int Occupancy { get; set; }
+    [Range(1,3000)]
+    public double RegularRate { get; set; }
+    public string Details { get; set; }
+    public string SqFt { get; set; }
+  }
+}
