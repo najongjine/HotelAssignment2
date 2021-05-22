39eb9379592fe4b209ca11c6ae5460a82bea565e
najongjine <najongjin3@hotmail.com>
2021-05-22 11:25:10 +0900

    .

---
+++ HotelAssignment2_API/Controllers/StripePaymentController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Models;
+using Stripe.Checkout;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelAssignment2_API.Controllers
+{
+  [Route("api/[controller]/[action]")]
+  [ApiController]
+  public class StripePaymentController : Controller
+  {
+    private readonly IConfiguration _configuration;
+
+    public StripePaymentController(IConfiguration configuration)
+    {
+      _configuration = configuration;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(StripePaymentDTO payment)
+    {
+      try
+      {
+        var domain = _configuration.GetValue<string>("HiddenVilla_Client_URL");
+        var option = new SessionCreateOptions
+        {
+          PaymentMethodTypes=new List<string>
+          {
+            "card"
+          }
+          ,LineItems=new List<SessionLineItemOptions>
+          {
+            new SessionLineItemOptions
+            {
+              PriceData=new SessionLineItemPriceDataOptions
+              {
+                UnitAmount=payment.Amount*100//convert to cents
+                ,Currency="USD"
+                ,ProductData=new SessionLineItemPriceDataProductDataOptions
+                {
+                  Name=payment.ProductName
+                }
+              }
+              ,Quantity=1
+            }
+          }
+          ,Mode="payment"
+          ,SuccessUrl=domain+"/success-payment?session_id={{CHECKOUT_SESSION_ID}}" //stripe return session id. automacally populates {{}} field
+          ,CancelUrl=domain+payment.ReturnUrl
+        };
+
+        var service = new SessionService();
+        Session session = await service.CreateAsync(option);
+
+        return Ok(new SuccessModel()
+        {
+          Data=session.Id
+        });
+      }catch(Exception e)
+      {
+        return BadRequest(new ErrorModel() { ErrorMessage=e.Message});
+      }
+    }
+  }
+}
