1bc69aabd35335ced7ba7dbdbaf170ea2be8d5f3
najongjine <najongjin3@hotmail.com>
2021-05-31 18:24:19 +0900

    .

---
+++ HiddenVilla_Client/Pages/Authentication/Login.razor.cs
@@ -0,0 +1,55 @@
+﻿using HiddenVilla_Client.Service.IService;
+using Microsoft.AspNetCore.Components;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace HiddenVilla_Client.Pages.Authentication
+{
+  public partial class Login
+  {
+    private AuthenticationDTO UserForAuthentication { get; set; } = new AuthenticationDTO();
+    public bool IsProcessing { get; set; } = false;
+    public bool ShowAuthenticationErrors { get; set; } = false;
+    public string Errors { get; set; }
+    public string ReturnUrl { get; set; }
+
+    [Inject]
+    public IAuthenticationService authenticationService { get; set; }
+    [Inject]
+    public NavigationManager navigationManager { get; set; }
+
+    private async Task LoginUser()
+    {
+      ShowAuthenticationErrors = false;
+      IsProcessing = true;
+      var result = await authenticationService.SignIn(UserForAuthentication);
+      if (result.IsAuthSuccessful)
+      {
+        IsProcessing = false;
+        var absouleUri = new Uri(navigationManager.Uri);
+        var queryParam = HttpUtility.ParseQueryString(absouleUri.Query);
+        ReturnUrl = queryParam["returnUrl"];
+        if (string.IsNullOrWhiteSpace(ReturnUrl))
+        {
+          navigationManager.NavigateTo("/");
+        }
+        else
+        {
+          navigationManager.NavigateTo("/"+ReturnUrl);
+        }
+        navigationManager.NavigateTo("/login");
+      }
+      else
+      {
+        IsProcessing = false;
+        Errors = result.ErrorMsg;
+        ShowAuthenticationErrors = true;
+      }
+    }
+
+  }
+}
--- HiddenVilla_Client/Pages/Authentication/Register.razor.cs
+++ HiddenVilla_Client/Pages/Authentication/Register.razor.cs
@@ -37,5 +37,6 @@ namespace HiddenVilla_Client.Pages.Authentication
         ShowRegistrationErrors = true;
       }
     }
+
   }
 }
